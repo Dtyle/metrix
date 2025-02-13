3f42fdfa0dcbbc257db2fed01cbf2f8d02184ebe
kirshi-dharani <dharani@kirshi.co>
2025-02-13 12:43:48 +0530

    add bussiest count

--- src/controller/analyticsController/get.controller.js
+++ src/controller/analyticsController/get.controller.js
@@ -2,22 +2,16 @@ const analyticsRepo = require("../../repository/analyticsRepo");
 const moment = require("moment");
 exports.getAnalytics = async (req, res) => {
     try {
-        const { date } = req.query;
+        let { date } = req.query;
 
-        if (!date) {
-            return res.status(400).json({
-                status: false,
-                message: "Please provide a date parameter.",
-            });
-        }
-
-        // Validate the date format
-        const moment = require("moment");
-        if (!moment(date, "YYYY-MM-DD", true).isValid()) {
-            return res.status(400).json({
-                status: false,
-                message: "Invalid date format. Please use YYYY-MM-DD.",
-            });
+        if (date) {
+            const moment = require("moment");
+            if (!moment(date, "YYYY-MM-DD", true).isValid()) {
+                return res.status(400).json({
+                    status: false,
+                    message: "Invalid date format. Please use YYYY-MM-DD.",
+                });
+            }
         }
 
         const analyticsData = await analyticsRepo.getAnalyticsData(req.sequelize, date);
@@ -36,4 +30,5 @@ exports.getAnalytics = async (req, res) => {
         });
     }
 };
+
 
